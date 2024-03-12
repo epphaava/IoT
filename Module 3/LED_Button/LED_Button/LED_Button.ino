2121967fe1ff6dadeb5e1df1117cfc6fa83ab12f
Epp <epp.haavasalu@ut.ee>
2024-03-12 14:27:23 +0200

    removed unnecessary includes

--- Module 3/LED_Button/LED_Button/LED_Button.ino
+++ Module 3/LED_Button/LED_Button/LED_Button.ino
@@ -1,14 +1,3 @@
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
-#include <ESP8266HTTPClient.h>
-
-#ifndef STASSID
-#define STASSID "iotempire-KEP"
-#define STAPSK "iotempireKEP"
-
-#endif
 // Pin connected to the button
 #define BUTTON_PIN D2
 
@@ -20,8 +9,6 @@ int buttonState = HIGH;
 // Variable to store the previous button state
 int lastButtonState = HIGH;
 
-const char* ssid = STASSID;
-const char* password = STAPSK;
 
 const int led = LED_BUILTIN;
 
